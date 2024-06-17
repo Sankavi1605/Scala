c0e978c229709d6d604b94362901c0724dc23275
Sankavi1605 <sankavithayaparan@gmail.com>
2024-06-17 15:08:34 +0530

    Q4 answer

--- Q4.scala
+++ Q4.scala
@@ -1,1 +1,23 @@
+object ex4{
+    val coverPrice=24.95
+    val discount=0.4
+    def calDiscount(price:Double,discount:Double):Double={
+    price*(1-discount)
+    
+    }
+    def calshippingCost(copies:Int):Double={
+        if(copies<=50){
+            3.00
+        }
+        else{
+            4+(copies-50)*0.75
+        }
+    }
+    def totalCost(copies:Int):Double={
+        copies*calDiscount(coverPrice,discount)+ calshippingCost(copies)
+    }
+   def main(args:Array[String]):Unit={
+   println(totalCost(60)) 
+   }
+}
 
